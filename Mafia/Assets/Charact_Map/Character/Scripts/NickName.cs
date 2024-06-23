7cdf421c907c79bb5555d749afad1c4a18b2b31f
IENFI <aien0118@yu.ac.kr>
2024-06-23 19:09:35 +0900

    Improve Level_1-Map
    - Level 1 맵 개선
    - 닉네임 널 값 오류 존재
    - 캐릭터 사이즈 2로 변경

--- Mafia/Assets/Charact_Map/Character/Scripts/NickName.cs
+++ Mafia/Assets/Charact_Map/Character/Scripts/NickName.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class NickName : MonoBehaviour
 {
+    private Camera mainCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
+        if (mainCamera == null)
+        {
+            Debug.LogError("Main camera not found. Please ensure there is a camera tagged as 'MainCamera'.");
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.forward = Camera.main.transform.forward;
+        if (mainCamera != null)
+        {
+            transform.forward = mainCamera.transform.forward;
+        }
     }
 }
