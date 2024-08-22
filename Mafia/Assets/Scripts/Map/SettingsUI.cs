66fa8bbf9180f5145a425a263aff1607408195d0
jiheon6835 <152905080+jihoen6835@users.noreply.github.com>
2024-08-22 13:31:17 +0900

    Revert "merge"
    This reverts commit 7cc276856b42fb9dccebfa6d2f730744f4729cee.

--- Mafia/Assets/Scripts/Map/SettingsUI.cs
+++ Mafia/Assets/Scripts/Map/SettingsUI.cs
@@ -2,50 +2,15 @@ using UnityEngine;
 
 public class SettingsUI : MonoBehaviour
 {
-
-    private PlayerController playerController;
-
-
-    private void Awake()
-    {
-        foreach (var pcc in FindObjectsOfType<PlayerController>())
-        {
-            if (pcc.photonView.IsMine)
-            {
-                playerController = pcc;
-                Debug.Log("SettingsUI에서 playerController를 찾았습니다.");
-                break;
-            }
-        }
-    }
-
     private void OnEnable()
     {
-        Debug.Log("Setting UI Enable");
-        if (!(GameManager.instance == null))
-        {
-            GameManager.instance.RegisterUIWindow(gameObject);
-            GameManager.instance.CheckUIState();
-        }
-        if (!(playerController == null))
-            playerController.EnableControl(false);
-
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+        GameManager.instance.RegisterUIWindow(gameObject);
+        GameManager.instance.CheckUIState();
     }
 
     private void OnDisable()
     {
-        Debug.Log("Setting UI Disable");
-        if (!(GameManager.instance == null))
-        {
-            GameManager.instance.UnregisterUIWindow(gameObject);
-            GameManager.instance.CheckUIState();
-        }
-        if (!(playerController == null))
-            playerController.EnableControl(true);
-
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        GameManager.instance.UnregisterUIWindow(gameObject);
+        GameManager.instance.CheckUIState();
     }
 }
