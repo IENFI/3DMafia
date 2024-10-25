c951559183af353ea0bb26405a61503105a0e056
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2024-10-25 15:55:48 +0900

    환경설정
    level_0에서 방 설정에 버튼 리스트 추가 했는데 안됨, 수정 바람 To 재혁

--- Mafia/Assets/Scripts/Setting/OpenSetting.cs
+++ Mafia/Assets/Scripts/Setting/OpenSetting.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using static UnityEngine.Rendering.DebugUI;
 
 public class OpenSetting : MonoBehaviour
 {
     public GameObject settingsPanel; // 설정 패널 (비활성화 상태로 시작)
-
+    
     void Update()
     {
+        Scene currentScene = SceneManager.GetActiveScene();
         // ESC 키가 눌렸을 때
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && currentScene.name == "Level_0")
         {
             ToggleSettingsPanel();
         }
@@ -19,5 +22,12 @@ public class OpenSetting : MonoBehaviour
     {
         // 패널의 활성화 상태를 토글
         settingsPanel.SetActive(!settingsPanel.activeSelf);
+    }
+
+    public void TogglePanel()
+    {
+        // 패널이 현재 활성화되어 있는지 확인한 후, 반대로 설정
+        bool isActive = settingsPanel.activeSelf;
+        settingsPanel.SetActive(!isActive);
     }
 }
